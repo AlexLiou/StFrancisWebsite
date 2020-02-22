99797b9d298daa1e08b95eafafb380da6b9492e7
Dillon McGovern <dillonm800@gmail.com>
2020-02-22 16:33:23 -0500

    added cards to staff page

--- src/App.js
+++ src/App.js
@@ -2,21 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Footer from './components/Footer/footer'
+import Header from './components/Header'
+import StaffPage from './pages/StaffPage'
 
 function App() {
   return (
-    // <div className="App">
-    //   <Router>
-    //     <div className="container">
-    //       <main>
-    //         <Switch>
-    //           <Route exact path="/homepage"
-    //         </Switch>
-    //       </main>
-    //     </div>
-    //   </Router>
-    // </div>
-    <Footer/>
+    <StaffPage/>
   );
 }
 
