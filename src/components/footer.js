e38cb84a7a15b9bb417b493d17e282b14f7f69c8
AlexLiou <alexliou0@gmail.com>
2020-02-22 18:02:53 -0500

    added contact page

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import '../styles/footer.css'
-import {Segment, Button} from 'semantic-ui-react'
+import {Segment, Button, Icon} from 'semantic-ui-react'
 
 const Footer = (props) => {
 
     return (
         <div className='footer'>
             <Segment className="center aligned">Copyright information</Segment>
+            <Button textAlign='right aligned' color='facebook' href='https://www.facebook.com/stfrancisgainesville/'>
+                <Icon name='facebook'/> Facebook
+            </Button>
         </div>
     );
 
